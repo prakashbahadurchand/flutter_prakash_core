66ec3ead8df4633ec08a2456014b116bbf02e3f9
Prakash Bahadur Chand <prakashbahadurchand@gmail.com>
2026-08-02 20:36:04 +0545

    refactor: remove FirebaseAppDistributionManager and its export from the core firebase library

--- lib/src/core/firebase/firebase.dart
+++ lib/src/core/firebase/firebase.dart
@@ -1,5 +1,4 @@
 export 'analytics/firebase_analytics_manager.dart';
-export 'app_distribution/firebase_app_distribution_manager.dart';
 export 'cloud_messaging/firebase_cloud_messaging_manager.dart';
 export 'crashlytics/firebase_crashlytics_manager.dart';
 export 'inapp_messaging/firebase_inapp_messaging_manager.dart';
