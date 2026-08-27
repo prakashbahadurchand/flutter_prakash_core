0113b8e0b1d4b66e4304cd5eebb89ed372f5d7f0
Prakash Bahadur Chand <prakashbahadurchand@gmail.com>
2026-08-28 03:13:34 +0545

    build: enable R8 code shrinking and configure release build settings in Android build.gradle

--- example/android/app/build.gradle.kts
+++ example/android/app/build.gradle.kts
@@ -15,11 +15,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID
-        // (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.prakashbahadurchand.flutter_prakash_core_example"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -28,10 +24,19 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
+    }
+
+    lint {
+        checkReleaseBuilds = false
+        abortOnError = false
     }
 
     flavorDimensions += "default"
@@ -43,14 +48,6 @@ android {
         create("dev") {
             dimension = "default"
             applicationIdSuffix = ".dev"
-            matchingFallbacks += listOf("staging")
-        }
-    }
-}
-
-dependencies { coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4") }
-
-flutter { source = "../.." }
             matchingFallbacks += listOf("staging")
         }
     }
