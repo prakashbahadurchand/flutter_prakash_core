cbd1273506bba781d16fc6b0135c23e3fa9f2a06
Prakash Bahadur Chand <prakashbahadurchand@gmail.com>
2026-09-07 21:19:15 +0545

    feat: configure build targets, ignore .fvm, and update environment constants and dependency injections

--- example/lib/core/di/injection.config.dart
+++ example/lib/core/di/injection.config.dart
@@ -12,7 +12,7 @@
 
 import 'package:dio/dio.dart' as _i361;
 import 'package:flutter_prakash_ads/flutter_prakash_ads.dart' as _i499;
-import 'package:flutter_prakash_core/fp_core.dart' as _i87;
+import 'package:flutter_prakash_core/fp_core.dart' as _i1069;
 import 'package:flutter_secure_storage/flutter_secure_storage.dart' as _i558;
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
@@ -155,10 +155,10 @@ extension GetItInjectableX on _i174.GetIt {
         gh<_i258.DashboardRemoteDataSource>(),
       ),
     );
-    gh.lazySingleton<_i87.ThemeCubit>(
+    gh.lazySingleton<_i1069.ThemeCubit>(
       () => registerModule.themeCubit(gh<_i460.SharedPreferences>()),
     );
-    gh.lazySingleton<_i87.LocaleCubit>(
+    gh.lazySingleton<_i1069.LocaleCubit>(
       () => registerModule.localeCubit(gh<_i460.SharedPreferences>()),
     );
     gh.factory<_i573.SettingsCubit>(
--- lib/src/blocs/paginated/paginated_state.freezed.dart
+++ lib/src/blocs/paginated/paginated_state.freezed.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // coverage:ignore-file
-// ignore_for_file: type=lint, type=warning, deprecated_member_use, deprecated_member_use_from_same_package
+// ignore_for_file: type=lint
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
 part of 'paginated_state.dart';
@@ -9,7 +9,6 @@ part of 'paginated_state.dart';
 // FreezedGenerator
 // **************************************************************************
 
-// GENERATED CODE - DO NOT MODIFY BY HAND
 // dart format off
 T _$identity<T>(T value) => value;
 /// @nodoc
@@ -64,7 +63,7 @@ class _$PaginatedStateCopyWithImpl<T,F,$Res>
 /// Create a copy of PaginatedState
 /// with the given fields replaced by the non-null parameter values.
 @pragma('vm:prefer-inline') @override $Res call({Object? searchQuery = null,Object? filter = freezed,Object? status = null,Object? totalLoaded = null,Object? isLastPage = null,}) {
-  return _then(PaginatedState(
+  return _then(_self.copyWith(
 searchQuery: null == searchQuery ? _self.searchQuery : searchQuery // ignore: cast_nullable_to_non_nullable
 as String,filter: freezed == filter ? _self.filter : filter // ignore: cast_nullable_to_non_nullable
 as F?,status: null == status ? _self.status : status // ignore: cast_nullable_to_non_nullable
