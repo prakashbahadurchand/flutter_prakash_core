fbb9ec780c29a537f9165a50437fce86e917cbaf
Prakash Bahadur Chand <prakashbahadurchand@gmail.com>
2026-09-15 22:31:15 +0545

    refactor: update generated freezed implementation for PaginatedState

--- lib/src/blocs/paginated/paginated_state.freezed.dart
+++ lib/src/blocs/paginated/paginated_state.freezed.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // coverage:ignore-file
-// ignore_for_file: type=lint
+// ignore_for_file: type=lint, type=warning, deprecated_member_use, deprecated_member_use_from_same_package
 // ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
 part of 'paginated_state.dart';
@@ -9,6 +9,7 @@ part of 'paginated_state.dart';
 // FreezedGenerator
 // **************************************************************************
 
+// GENERATED CODE - DO NOT MODIFY BY HAND
 // dart format off
 T _$identity<T>(T value) => value;
 /// @nodoc
@@ -25,16 +26,21 @@ $PaginatedStateCopyWith<T, F, PaginatedState<T, F>> get copyWith => _$PaginatedS
 
 @override
 bool operator ==(Object other) {
-  return identical(this, other) || (other.runtimeType == runtimeType&&other is PaginatedState<T, F>&&(identical(other.searchQuery, searchQuery) || other.searchQuery == searchQuery)&&const DeepCollectionEquality().equals(other.filter, filter)&&(identical(other.status, status) || other.status == status)&&(identical(other.totalLoaded, totalLoaded) || other.totalLoaded == totalLoaded)&&(identical(other.isLastPage, isLastPage) || other.isLastPage == isLastPage));
+  final _this = this as PaginatedState<T, F>;
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is PaginatedState<T, F>&&(identical(other.searchQuery, _this.searchQuery) || other.searchQuery == _this.searchQuery)&&const DeepCollectionEquality().equals(other.filter, _this.filter)&&(identical(other.status, _this.status) || other.status == _this.status)&&(identical(other.totalLoaded, _this.totalLoaded) || other.totalLoaded == _this.totalLoaded)&&(identical(other.isLastPage, _this.isLastPage) || other.isLastPage == _this.isLastPage));
 }
 
 
 @override
-int get hashCode => Object.hash(runtimeType,searchQuery,const DeepCollectionEquality().hash(filter),status,totalLoaded,isLastPage);
+int get hashCode {
+  final _this = this as PaginatedState<T, F>;
+  return Object.hash(runtimeType,_this.searchQuery,const DeepCollectionEquality().hash(_this.filter),_this.status,_this.totalLoaded,_this.isLastPage);
+}
 
 @override
 String toString() {
-  return 'PaginatedState<$T, $F>(searchQuery: $searchQuery, filter: $filter, status: $status, totalLoaded: $totalLoaded, isLastPage: $isLastPage)';
+  final _this = this as PaginatedState<T, F>;
+  return 'PaginatedState<$T, $F>(searchQuery: ${_this.searchQuery}, filter: ${_this.filter}, status: ${_this.status}, totalLoaded: ${_this.totalLoaded}, isLastPage: ${_this.isLastPage})';
 }
 
 
@@ -63,7 +69,7 @@ class _$PaginatedStateCopyWithImpl<T,F,$Res>
 /// Create a copy of PaginatedState
 /// with the given fields replaced by the non-null parameter values.
 @pragma('vm:prefer-inline') @override $Res call({Object? searchQuery = null,Object? filter = freezed,Object? status = null,Object? totalLoaded = null,Object? isLastPage = null,}) {
-  return _then(_self.copyWith(
+  return _then(PaginatedState(
 searchQuery: null == searchQuery ? _self.searchQuery : searchQuery // ignore: cast_nullable_to_non_nullable
 as String,filter: freezed == filter ? _self.filter : filter // ignore: cast_nullable_to_non_nullable
 as F?,status: null == status ? _self.status : status // ignore: cast_nullable_to_non_nullable
@@ -229,16 +235,18 @@ _$PaginatedStateCopyWith<T, F, _PaginatedState<T, F>> get copyWith => __$Paginat
 
 @override
 bool operator ==(Object other) {
-  return identical(this, other) || (other.runtimeType == runtimeType&&other is _PaginatedState<T, F>&&(identical(other.searchQuery, searchQuery) || other.searchQuery == searchQuery)&&const DeepCollectionEquality().equals(other.filter, filter)&&(identical(other.status, status) || other.status == status)&&(identical(other.totalLoaded, totalLoaded) || other.totalLoaded == totalLoaded)&&(identical(other.isLastPage, isLastPage) || other.isLastPage == isLastPage));
+    return identical(this, other) || (other.runtimeType == runtimeType&&other is _PaginatedState<T, F>&&(identical(other.searchQuery, searchQuery) || other.searchQuery == searchQuery)&&const DeepCollectionEquality().equals(other.filter, filter)&&(identical(other.status, status) || other.status == status)&&(identical(other.totalLoaded, totalLoaded) || other.totalLoaded == totalLoaded)&&(identical(other.isLastPage, isLastPage) || other.isLastPage == isLastPage));
 }
 
 
 @override
-int get hashCode => Object.hash(runtimeType,searchQuery,const DeepCollectionEquality().hash(filter),status,totalLoaded,isLastPage);
+int get hashCode {
+    return Object.hash(runtimeType,searchQuery,const DeepCollectionEquality().hash(filter),status,totalLoaded,isLastPage);
+}
 
 @override
 String toString() {
-  return 'PaginatedState<$T, $F>(searchQuery: $searchQuery, filter: $filter, status: $status, totalLoaded: $totalLoaded, isLastPage: $isLastPage)';
+    return 'PaginatedState<$T, $F>(searchQuery: $searchQuery, filter: $filter, status: $status, totalLoaded: $totalLoaded, isLastPage: $isLastPage)';
 }
 
 
